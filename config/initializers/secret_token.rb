874f38e2ba5599fe3fe2bb14ec70044aa18a4404
Charlie Moseley <charlie@robopengu.in>
2012-01-12 10:28:47 -0800

    Clean Rails 3.1.3 install with -TG

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emotejiji::Application.config.secret_token = 'f8dde3d8ed46e07a6b9c52ac7c91dd6af167d77ac1d51ec09be7ae006e588cad364fe0f9f2c5c7bfa792dbc14b1600c601670128ae6aac9ff42a83421a2a1adf'
