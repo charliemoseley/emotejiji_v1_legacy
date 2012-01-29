3bcaf702e0774421bd805775e2cb399871430538
Charlie Moseley <charlie@robopengu.in>
2012-01-29 15:52:04 -0800

    Implemented a sorter class so that emotes always show up in a nice grid.

--- app/helpers/emotes_helper.rb
+++ app/helpers/emotes_helper.rb
@@ -1,2 +1,9 @@
 module EmotesHelper
+  
+  # Used to increase the font size on unicode graphic emoticons
+  def custom_styles(emote)
+    if (1..38).include? emote.id
+      'style="font-size: 40px;"'
+    end
+  end
 end
---
+++ app/renderers/emote_list_sorter.rb
@@ -0,0 +1,59 @@
+class EmoteListSorter
+  attr_accessor :emote_list, :list_length
+  
+  def initialize(emote_list, list_length = 4)
+    @emote_list  = emote_list
+    @list_length = list_length
+    
+    @position = 0
+    @overflow = Array.new
+    @return   = Array.new
+  end
+  
+  def sort
+    render_list
+    @return
+  end
+  
+  def pass_through
+    @emote_list
+  end
+  
+  private
+  
+  def render_list
+    @emote_list.each do |emote|
+      # First check if the overflow array has any emotes
+      unless @overflow.length == 0
+        # If so loop over them
+        @overflow.each_with_index do |oemote, index|
+          # Check if they can be positioned into the grid
+          unless @position + oemote.width >  @list_length
+            # If so, add it to the grid, remove it from the overflow
+            @return.push oemote
+            @overflow.delete_at(index)
+            
+            # Update the position and do a position check
+            @position = @position + oemote.width
+            if @position ==  @list_length
+              @position = 0 
+            end
+          end
+        end
+      end
+      
+      # Do the same thing with the current emote except if an
+      # overflow occurs, put that emote into the overflow array
+      unless @position + emote.width >  @list_length
+        @return.push emote
+        
+        @position = @position + emote.width
+        if @position ==  @list_length
+          @position = 0 
+        end
+      else
+        @overflow.push emote
+      end
+    end
+  end
+end
