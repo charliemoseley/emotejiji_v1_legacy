6ee53977b0f5b9d12a5da0e403f0db635ebcac0e
Charlie Moseley <charlie@robopengu.in>
2012-01-30 02:15:13 -0800

    Reogranized JS file a bit.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -62,10 +62,10 @@ $(document).ready(function() {
   // This is really dumb, but might be the only way to pull this off
   $('#sort-list li').live('click', function() {
     // ToDo: Function order is important here as change_sort needs to fire off
-    // before animate_sort_list.  Probably worth updating change_sort so
-    // it fires off animate_sort_list after its complete.
+    // before animateSortList.  Probably worth updating change_sort so
+    // it fires off animateSortList after its complete.
     change_sort($(this));
-    animate_sort_list($(this));
+    animateSortList($(this));
     
   });
   
@@ -75,217 +75,14 @@ $(document).ready(function() {
   }
 });
 
-change_sort = function($selected) {
-  if($selected.hasClass('active')) {
-    return true;
-  }
-  
-  selected_sort = $selected.text();
-  current_display = $('#current-display').text();
-  
-  switch(current_display) {
-    case 'all':
-      $('#link-home').data('params', { sort: selected_sort });
-      $('#link-home').click();
-      break;
-    case 'search':
-      tagSearch('', selected_sort);
-      break;
-  }
-}
-
-animate_sort_list = function($selected) {
-  $container = $('#sort-list');
-  $list      = $('#sort-list li');
-  $active    = $('#sort-list li.active');
-  $inactive1 = $('#sort-list li.inactive:first');
-  $inactive2 = $('#sort-list li.inactive:last');
-  
-  check_value = parseInt($inactive1.css('right'));
-  // Check to see if the element has been position
-  if(isNaN(check_value) || (check_value < 0)) {
-    // Prep the off screen locations of two inactive li
-    $inactive1.css('right', -$inactive1.outerWidth());
-    $inactive2.css('right', -$inactive1.outerWidth() + -$inactive2.outerWidth());
-    
-    // This allows both to move linear while keeping the appearance that they are together
-    $inactive1.animate({'right': $active.outerWidth() + $inactive2.outerWidth()}, 200, 'linear');
-    $inactive2.animate({'right': $active.outerWidth()}, 200, 'linear');
-  } else {
-    // Check first if the newly selected active li is not the same as the old one
-    if($selected.hasClass('active')) {
-      $inactive1.animate({'right': -$inactive1.outerWidth()}, 200, 'linear', function() { $(this).removeAttr('style'); });
-      $inactive2.animate({'right': -$inactive1.outerWidth() + -$inactive2.outerWidth()}, 200, 'linear', function() { $selected.removeAttr('style'); })
-    } else {
-      // Reassign the classes and temporarily absolutely position the active class
-      $right = $active.css('right', 0).removeClass().addClass('inactive');
-      $active = $selected.css('position', 'absolute').removeClass().addClass('active');
-      
-      // Determine which inactive li was not convereted to the active class
-      if(parseInt($active.css('right')) == parseInt($inactive1.css('right'))) {
-        $unknown = $inactive2;
-      } else {
-        $unknown = $inactive1;
-      }
-      
-      // Figure out the left and middle position lis in addition to determine
-      // the position of the active li
-      is_middle_active = true;
-      if(parseInt($active.css('right')) > parseInt($unknown.css('right'))) {
-        $left = $active;
-        $middle = $unknown;
-        is_middle_active = false;
-      } else {
-        $left = $unknown;
-        $middle = $active;
-      }
-      
-      // To give the illusion that the element on the far right is sliding
-      // undeneath is own padding, we temporarily create an absolutely positioned
-      // div to cover the text
-      $container.append('<li style="position: absolute; right: 0;"></li>');
-      $cover = $container.children().last();
-      $cover.css('width', $right.css('padding')).css('height', $right.css('height')).css('background-color', $active.css('background-color')).css('z-index',  parseInt($unknown.css('z-index')) + 1);
-      
-      // To garuantee that the middle li gracefully slides over the cover, we
-      // increase it's z-index (assuming it's the active li)
-      if(is_middle_active) {
-        $middle.css('z-index', parseInt($cover.css('z-index')) + 1);
-      }
-      
-      // Animate everything to the 0 position knowning the active class will
-      // float to the top.  Have the right most li animate off the screen (since
-      // we know it can never be the active li).
-      $left.animate({'right': 0}, 200, 'linear', function() { $list.removeAttr('style'); });
-      // If the middle is not the active li, it needs to slide off the screen.
-      $middle.animate({'right': 0}, 100, 'linear', function() {
-        if(!is_middle_active) {
-          // The additioinal remoteAttr on this guy is because you not garuanteed that the $left.animate will finish last
-          // so you need to clear it again to make sure the styles are removed on all the lis
-          $middle.animate({'right': -$middle.outerWidth()}, 100, 'linear', function() { $cover.remove(); $list.removeAttr('style');  });
-        }
-      });
-      $right.animate({'right': -$right.outerWidth()}, 100, 'linear', function() { if(is_middle_active) { $cover.remove(); } })
-    }
-  }
-}
-
-/**
- * @description A function to handle the display and redisplay of tags on the site.
- * @param $tagContainer A jQuery DOM object that is the ul that will hold the tag list
- * @param tagList This can either be an array or a string.  If provided an array, then it will render the array as tags verbatim in the provided order.  If a string is passed, a duplication and empty check will be performed.  If the string passes both of those, then it will be appended to the tag list generated by parses through the $tagContainer.
- * @param Specifies whether you want the noLink css class applied to the tags.  Defaults to false.
- */
-display_tags = function($tagContainer, tagList, noLink) {
-  var tempArr = [];
-  
-  // Set default value on no link
-  if(noLink == null) noLink = false;
-  
-  // Check if tagList was passed in a string and build an
-  // array for it if it was.
-  if(isObjectType(tagList, 'string')) {
-    $tagContainer.find('a').each(function() {
-      tempArr.push($(this).text());
-    });
-    
-    // Run an empty and duplication check on the tag and add it to the array
-    // if it passes.
-    if($.trim(tagList) != '') {
-      if($.inArray(tagList, tempArr) == -1) tempArr.push(tagList);
-    }
-    tagList = tempArr;
-  }
-  $tagContainer.empty();
-  $.each(tagList, function(index, value) {
-    if(noLink) { 
-      $tagContainer.append('<li><a class="no-link">' + value + '</a></li>');
-    } else {
-      $tagContainer.append('<li><a>' + value + '</a></li>');
-    }
-  });
-}
-
-
-/**
- *  @description A simple function that determines whether a given tag is in a tag list
- */
-isDuplicateTag = function(tag, tagList) {
-  if(!isObjectType(tagList, 'array')) tagList = buildTagList(tagList);
-  if( $.inArray(tag, tagList) == -1 ) return false;
-  return true;
-}
-
-/**
- *  @description A simple function to build a taglist from a jQuery object
- */
-buildTagList = function($tagContainer) {
-  var tagList = [];
-  
-  $tagContainer.find('a').each(function() {
-    tagList.push($(this).text());
-  });
-  return tagList;
-}
-
-// ToDo: Clean up this and really tighten up the selectors (heck, abstract it out better too)
-tagSearch = function(tag, sort) {
-  if(sort == null) sort = currentSort();
-  var tagList;
-  
-  // Build up the tag list and see if we even search
-  tagList = buildTagList($('#search-tags'));
-  if(isDuplicateTag(tag, tagList)) return false;
-  //if(tag == '' && tagList.length == 0) return false;
-  
-  $('#loading').show();
-  $search = $('#search');
-  
-  error = false;
-  $.post('/emotes/search', { tag: tag, tag_list: tagList, sort: sort }, function(data) {
-    switch(data.status) {
-      case 'invalid_tag':
-        $search.attr('disabled', 'disabled');
-        $search.attr('style', 'background-color: red; color: #FFFFFF;');
-        $search.val("Sorry, that tag doesn't exist.");
-        $search.animate({ backgroundColor: '#FFFFFF' }, 1500, 'easeInCubic', function() {
-          $search.val('');
-          $search.removeAttr('disabled');
-          $search.removeAttr('style');
-        });
-        error = true;
-      break;
-      case 'valid_results':
-      case 'no_results':
-      case 'reset_results':
-        tagList.push(tag);
-        $('#content').html(data.view);
-        if(tag.length != 0) display_tags($('#search-tags'), tagList);
-      break;
-    }
-    $('#loading').hide();
-    // Sometimes autocomplete doesnt disappear if you type in the whole word and hit
-    // enter.  By hiding it, we garuntee that it goes away.
-    $('.ui-autocomplete').hide();
-    if(!error) {
-      $search.val('');
-    }
-  });
-  
-  $search.val('');
-}
-
+/***************************************************************
+ * Initial Setup and Binders
+ ***************************************************************/
 setup_links = function() {
-  default_remote_link($('#link-favorites'));
-  default_remote_link($('#link-recent'));
-  default_remote_link($('#link-home'));
-  default_remote_link($('#link-profile'));
-  
-  /* // Incase I ever need to bind on the autocomplete, this is how to do it [below]
-  $('#add_tags').bind('railsAutocomplete.select', function(event, data) {
-    console.log('Data:' + data.item.name);
-  });
-  */
+  defaultRemoteLink($('#link-favorites'));
+  defaultRemoteLink($('#link-recent'));
+  defaultRemoteLink($('#link-home'));
+  defaultRemoteLink($('#link-profile'));
 }
 
 setup_new_emote_form = function() {
@@ -399,7 +196,7 @@ setup_forms = function() {
     });
 }
 
-default_remote_link = function($target) {
+defaultRemoteLink = function($target) {
   var $l = $('#loading');
   $target.bind('ajax:beforeSend', function(event, xhr, settings) { $l.show(); })
          .bind('ajax:success',    function(event, data, status, xhr) 
@@ -408,7 +205,137 @@ default_remote_link = function($target) {
          .bind('ajax:error',      function(event, xhr, status, error)
             { /* ToDo: Create an error message sometime here */ });
 }
+ 
+/***************************************************************
+ * Tag Related Functions
+ ***************************************************************/
 
+/**
+ * @description A function to handle the display and redisplay of tags on the site.
+ * @param $tagContainer A jQuery DOM object that is the ul that will hold the tag list
+ * @param tagList This can either be an array or a string.  If provided an array, then it will render the array as tags verbatim in the provided order.  If a string is passed, a duplication and empty check will be performed.  If the string passes both of those, then it will be appended to the tag list generated by parses through the $tagContainer.
+ * @param Specifies whether you want the noLink css class applied to the tags.  Defaults to false.
+ */
+displayTags = function($tagContainer, tagList, noLink) {
+  var tempArr = [];
+  
+  // Set default value on no link
+  if(noLink == null) noLink = false;
+  
+  // Check if tagList was passed in a string and build an
+  // array for it if it was.
+  if(isObjectType(tagList, 'string')) {
+    $tagContainer.find('a').each(function() {
+      tempArr.push($(this).text());
+    });
+    
+    // Run an empty and duplication check on the tag and add it to the array
+    // if it passes.
+    if($.trim(tagList) != '') {
+      if($.inArray(tagList, tempArr) == -1) tempArr.push(tagList);
+    }
+    tagList = tempArr;
+  }
+  $tagContainer.empty();
+  $.each(tagList, function(index, value) {
+    if(noLink) { 
+      $tagContainer.append('<li><a class="no-link">' + value + '</a></li>');
+    } else {
+      $tagContainer.append('<li><a>' + value + '</a></li>');
+    }
+  });
+}
+
+// ToDo: Clean up this and really tighten up the selectors (heck, abstract it out better too)
+tagSearch = function(tag, sort) {
+  if(sort == null) sort = currentSort();
+  var tagList;
+  
+  // Build up the tag list and see if we even search
+  tagList = buildTagList($('#search-tags'));
+  if(isDuplicateTag(tag, tagList)) return false;
+  //if(tag == '' && tagList.length == 0) return false;
+  
+  $('#loading').show();
+  $search = $('#search');
+  
+  error = false;
+  $.post('/emotes/search', { tag: tag, tag_list: tagList, sort: sort }, function(data) {
+    switch(data.status) {
+      case 'invalid_tag':
+        $search.attr('disabled', 'disabled');
+        $search.attr('style', 'background-color: red; color: #FFFFFF;');
+        $search.val("Sorry, that tag doesn't exist.");
+        $search.animate({ backgroundColor: '#FFFFFF' }, 1500, 'easeInCubic', function() {
+          $search.val('');
+          $search.removeAttr('disabled');
+          $search.removeAttr('style');
+        });
+        error = true;
+      break;
+      case 'valid_results':
+      case 'no_results':
+      case 'reset_results':
+        tagList.push(tag);
+        $('#content').html(data.view);
+        if(tag.length != 0) displayTags($('#search-tags'), tagList);
+      break;
+    }
+    $('#loading').hide();
+    // Sometimes autocomplete doesnt disappear if you type in the whole word and hit
+    // enter.  By hiding it, we garuntee that it goes away.
+    $('.ui-autocomplete').hide();
+    if(!error) {
+      $search.val('');
+    }
+  });
+  
+  $search.val('');
+}
+
+/**
+ *  @description A simple function to build a taglist from a jQuery object
+ */
+buildTagList = function($tagContainer) {
+  var tagList = [];
+  
+  $tagContainer.find('a').each(function() {
+    tagList.push($(this).text());
+  });
+  return tagList;
+}
+
+/**
+ *  @description A simple function that determines whether a given tag is in a tag list
+ */
+isDuplicateTag = function(tag, tagList) {
+  if(!isObjectType(tagList, 'array')) tagList = buildTagList(tagList);
+  if( $.inArray(tag, tagList) == -1 ) return false;
+  return true;
+}
+
+/***************************************************************
+ * Other Functions
+ ***************************************************************/
+change_sort = function($selected) {
+  if($selected.hasClass('active')) {
+    return true;
+  }
+  
+  selected_sort = $selected.text();
+  current_display = $('#current-display').text();
+  
+  switch(current_display) {
+    case 'all':
+      $('#link-home').data('params', { sort: selected_sort });
+      $('#link-home').click();
+      break;
+    case 'search':
+      tagSearch('', selected_sort);
+      break;
+  }
+}
+ 
 emoticon_clicked = function($container) {
   var emoticon, note, id, $form;
   $form = $('#emoticon-display form');
@@ -424,7 +351,7 @@ emoticon_clicked = function($container) {
   $('#selected-id').val(id);
   $('#selected-emoticon').text(emoticon);
   $('#selected-note').text(note);
-  display_tags($('#tag-list'), tag_list);
+  displayTags($('#tag-list'), tag_list);
   update_recent_emotes(id);
   
   if($('#emoticon-display').is(':hidden')) {
@@ -447,7 +374,7 @@ new_emote_add_tag = function ($input, $tagList) {
   $input.val('');
   
   $input.parent().children('span').hide();
-  display_tags($tagList, tag);
+  displayTags($tagList, tag);
   
 }
 
@@ -482,4 +409,83 @@ isObjectType = function(variable, type) {
   
   if(Object.prototype.toString.call(variable) === type) return true;
   return false;
+}
+
+/***************************************************************
+ * Animation
+ ***************************************************************/
+animateSortList = function($selected) {
+  $container = $('#sort-list');
+  $list      = $('#sort-list li');
+  $active    = $('#sort-list li.active');
+  $inactive1 = $('#sort-list li.inactive:first');
+  $inactive2 = $('#sort-list li.inactive:last');
+  
+  check_value = parseInt($inactive1.css('right'));
+  // Check to see if the element has been position
+  if(isNaN(check_value) || (check_value < 0)) {
+    // Prep the off screen locations of two inactive li
+    $inactive1.css('right', -$inactive1.outerWidth());
+    $inactive2.css('right', -$inactive1.outerWidth() + -$inactive2.outerWidth());
+    
+    // This allows both to move linear while keeping the appearance that they are together
+    $inactive1.animate({'right': $active.outerWidth() + $inactive2.outerWidth()}, 200, 'linear');
+    $inactive2.animate({'right': $active.outerWidth()}, 200, 'linear');
+  } else {
+    // Check first if the newly selected active li is not the same as the old one
+    if($selected.hasClass('active')) {
+      $inactive1.animate({'right': -$inactive1.outerWidth()}, 200, 'linear', function() { $(this).removeAttr('style'); });
+      $inactive2.animate({'right': -$inactive1.outerWidth() + -$inactive2.outerWidth()}, 200, 'linear', function() { $selected.removeAttr('style'); })
+    } else {
+      // Reassign the classes and temporarily absolutely position the active class
+      $right = $active.css('right', 0).removeClass().addClass('inactive');
+      $active = $selected.css('position', 'absolute').removeClass().addClass('active');
+      
+      // Determine which inactive li was not convereted to the active class
+      if(parseInt($active.css('right')) == parseInt($inactive1.css('right'))) {
+        $unknown = $inactive2;
+      } else {
+        $unknown = $inactive1;
+      }
+      
+      // Figure out the left and middle position lis in addition to determine
+      // the position of the active li
+      is_middle_active = true;
+      if(parseInt($active.css('right')) > parseInt($unknown.css('right'))) {
+        $left = $active;
+        $middle = $unknown;
+        is_middle_active = false;
+      } else {
+        $left = $unknown;
+        $middle = $active;
+      }
+      
+      // To give the illusion that the element on the far right is sliding
+      // undeneath is own padding, we temporarily create an absolutely positioned
+      // div to cover the text
+      $container.append('<li style="position: absolute; right: 0;"></li>');
+      $cover = $container.children().last();
+      $cover.css('width', $right.css('padding')).css('height', $right.css('height')).css('background-color', $active.css('background-color')).css('z-index',  parseInt($unknown.css('z-index')) + 1);
+      
+      // To garuantee that the middle li gracefully slides over the cover, we
+      // increase it's z-index (assuming it's the active li)
+      if(is_middle_active) {
+        $middle.css('z-index', parseInt($cover.css('z-index')) + 1);
+      }
+      
+      // Animate everything to the 0 position knowning the active class will
+      // float to the top.  Have the right most li animate off the screen (since
+      // we know it can never be the active li).
+      $left.animate({'right': 0}, 200, 'linear', function() { $list.removeAttr('style'); });
+      // If the middle is not the active li, it needs to slide off the screen.
+      $middle.animate({'right': 0}, 100, 'linear', function() {
+        if(!is_middle_active) {
+          // The additioinal remoteAttr on this guy is because you not garuanteed that the $left.animate will finish last
+          // so you need to clear it again to make sure the styles are removed on all the lis
+          $middle.animate({'right': -$middle.outerWidth()}, 100, 'linear', function() { $cover.remove(); $list.removeAttr('style');  });
+        }
+      });
+      $right.animate({'right': -$right.outerWidth()}, 100, 'linear', function() { if(is_middle_active) { $cover.remove(); } })
+    }
+  }
 }
