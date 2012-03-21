9e52dd61dc7789673f1f6c277f63069c98dcb062
Charlie Moseley <charlie@robopengu.in>
2012-03-20 23:31:04 -0700

    Yet another attempt =_= (redis heroku).

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,7 @@
 require 'redis'
 require 'redis/objects'
 
-uri = URI.parse(REDIS_URI)
+redis_uri = Rails.env.production? ? ENV["REDISTOGO_URL"] : 'redis://localhost:6379' 
+uri = URI.parse(redis_uri)
+
 Redis.current = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
