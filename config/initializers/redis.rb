4b9fc0b6926bb8b40062e773c63704eb0134b941
Charlie Moseley <charlie@robopengu.in>
2012-03-21 00:27:01 -0700

    blargh.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,12 @@
-require 'redis'
-require 'redis/objects'
+# require 'redis'
+# require 'redis/objects'
 
-uri = URI.parse(ENV["REDISTOGO_URL"])
-
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-Redis.current = REDIS
+if url = ENV['REDISTOGO_URL']
+  uri = URI.parse(url)
+  Redis.current = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+elsif Rails.env.development? or Rails.env.test?  # or check ENV['RACK_ENV']
+  Redis.current = Redis.new  # localhost
+else
+  # supposed to have a server
+  raise "Missing redis server - please set REDISTOGO_URL env var"
+end
