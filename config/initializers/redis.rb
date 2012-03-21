f7f5eb5ff76b70b595ec399271efe20ccd55bc5c
Charlie Moseley <charlie@robopengu.in>
2012-03-20 23:34:50 -0700

    heroku log viewing

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,6 +2,8 @@ require 'redis'
 require 'redis/objects'
 
 redis_uri = Rails.env.production? ? ENV["REDISTOGO_URL"] : 'redis://localhost:6379' 
+Rails.logger.info 'Errno::[' + ENV["REDISTOGO_URL"] + ']'
+Rails.logger.info 'Errno::URI[' + redis_uri + ']'
 uri = URI.parse(redis_uri)
 
 Redis.current = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
