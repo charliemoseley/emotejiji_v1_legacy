fd876cdd27ef684b96782fa7b6872170c3831e80
Charlie Moseley <charlie@robopengu.in>
2012-03-21 01:21:39 -0700

    Straight redis to go connection.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,7 +2,7 @@
 # require 'redis/objects'
 
 
-uri = URI.parse("redis://redistogo:7138590ae359496101b83508d065a466@cod.redistogo.com:9935")
+uri = URI.parse("redis://koreanwalker:a42b823a34fde7bcce8d2497605692d0@pike.redistogo.com:9258/")
 Redis.current = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 # if url = ENV['REDISTOGO_URL']
