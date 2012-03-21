36efec361d28f6ff81454c027999662574b8d524
Charlie Moseley <charlie@robopengu.in>
2012-03-20 23:47:55 -0700

    Sigh...

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,8 +2,7 @@ require 'redis'
 require 'redis/objects'
 
 redis_uri = Rails.env.production? ? ENV["REDISTOGO_URL"] : 'redis://localhost:6379' 
-Rails.logger.info 'Errno::[' + ENV["REDISTOGO_URL"] + ']'
-Rails.logger.info 'Errno::URI[' + redis_uri + ']'
 uri = URI.parse(redis_uri)
 
-Redis.current = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+Redis.current = REDIS
