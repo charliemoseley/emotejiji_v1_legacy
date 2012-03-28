649b4a374b9e34fe47a73a40b302ee4cac3de69a
Charlie Moseley <charlie@robopengu.in>
2012-03-27 18:39:55 -0700

    Testing standard redis gem to see if it writes properly on heroku.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,16 +1,16 @@
-# require 'redis'
-# require 'redis/objects'
+require 'redis'
+require 'redis/objects'
 
 
-uri = URI.parse("redis://koreanwalker:a42b823a34fde7bcce8d2497605692d0@pike.redistogo.com:9258/")
-Redis.current = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+# uri = URI.parse("redis://koreanwalker:a42b823a34fde7bcce8d2497605692d0@pike.redistogo.com:9258/")
+# Redis.current = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
-# if url = ENV['REDISTOGO_URL']
-#   uri = URI.parse(url)
-#   Redis.current = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-# elsif Rails.env.development? or Rails.env.test?  # or check ENV['RACK_ENV']
-#   Redis.current = Redis.new  # localhost
-# else
-#   # supposed to have a server
-#   raise "Missing redis server - please set REDISTOGO_URL env var"
-# end
+if url = ENV['REDISTOGO_URL']
+  uri = URI.parse(url)
+  Redis.current = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+elsif Rails.env.development? or Rails.env.test?  # or check ENV['RACK_ENV']
+  Redis.current = Redis.new  # localhost
+else
+  # supposed to have a server
+  raise "Missing redis server - please set REDISTOGO_URL env var"
+end
