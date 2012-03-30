fb0755ab89b816382715846a88d20dd39e92556e
Charlie Moseley <charlie@robopengu.in>
2012-03-29 18:07:18 -0700

    Added in local redis.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,6 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+if Rails.env.development? or Rails.env.test?
+  REDIS = Redis.new  # localhost
+else
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
