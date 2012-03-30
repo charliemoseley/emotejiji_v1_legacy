36b0920e70e84984e0ac64c0e09388a4e350be23
Charlie Moseley <charlie@robopengu.in>
2012-03-29 17:59:15 -0700

    Copy pasta heroku redis config.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,15 +1,2 @@
-require 'redis'
-require 'redis/objects'
-
-
-# uri = URI.parse("redis://koreanwalker:a42b823a34fde7bcce8d2497605692d0@pike.redistogo.com:9258/")
-# Redis.current = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-
-if url = ENV['REDISTOGO_URL']
-  REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
-elsif Rails.env.development? or Rails.env.test?  # or check ENV['RACK_ENV']
-  REDIS = Redis.new  # localhost
-else
-  # supposed to have a server
-  raise "Missing redis server - please set REDISTOGO_URL env var"
-end
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
